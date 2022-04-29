b0e53d1d445bd055097c2d6820768819c0f79608
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-04-29 14:54:47 +0200

    init

--- components/Blocks/HeroBlock/Hero.tsx
+++ components/Blocks/HeroBlock/Hero.tsx
@@ -1,3 +1,4 @@
+import Button from "@components/Button/Button";
 import Typo from "@components/Typography/Typography";
 import SanityImage from "@lib/SanityImage";
 import React from "react";
@@ -8,8 +9,6 @@ interface HeroProps extends HeroBlogResult {}
 
 const Hero: React.FC<HeroProps> = (props) => {
   const { header, image } = props;
-
-  console.log(header);
 
   return (
     <div
@@ -24,8 +23,11 @@ const Hero: React.FC<HeroProps> = (props) => {
           objectPosition={"left"}
         />
       </div>
-      <div className=" px-4 flex justify-center items-center col-start-1 md:col-start-1 col-span-full md:col-span-3 z-10 row-span-1 row-start-2 md:row-start-1 ">
-        <Typo variant={"h1"}>{header}</Typo>
+      <div className=" px-4 flex  justify-center items-center col-start-1 md:col-start-1 col-span-full md:col-span-3 z-10 row-span-1 row-start-2 md:row-start-1 ">
+        <div>
+          <Typo variant={"h1"}>{header}</Typo>
+          <Button>Apply Now</Button>
+        </div>
       </div>
     </div>
   );

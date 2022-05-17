823caa55b3c5b591890041f0f927886fde7682b4
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-05-17 11:54:16 +0200

    smaler image credit

--- components/Blocks/SectionBlock/SectionBlock.tsx
+++ components/Blocks/SectionBlock/SectionBlock.tsx
@@ -51,7 +51,9 @@ const SectionBlock: React.FC<SectionBlockProps> = (props) => {
           >
             <SanityImage image={image} layout="responsive" />
             {image.credit && (
-              <div className="text-right mr-5 opacity-60">{image.credit}</div>
+              <div className="text-right mr-5 opacity-60 text-sm">
+                {image.credit}
+              </div>
             )}
           </div>
         )}
